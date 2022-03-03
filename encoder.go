8b8d5e8c57605df54f92cfcfb07356609c8b5347
Phil Eaton <phil@eatonphil.com>
2022-03-03 21:44:33 +0000

    Support for streaming

--- encoder.go
+++ encoder.go
@@ -3,13 +3,121 @@ package json
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 	"strconv"
 
 	goccy_json "github.com/goccy/go-json"
 )
 
-func EncodeGeneric(out io.Writer, obj interface{}, marshalFn func(o interface{}) ([]byte, error)) error {
+type Marshaler func(interface{}) ([]byte, error)
+
+type StreamEncoder struct {
+	buf *bytes.Buffer
+	w     io.Writer
+	first bool
+	array bool
+	quotedColumns map[string][]byte
+	marshalFn Marshaler
+}
+
+func NewStreamEncoder(w io.Writer, marshalFn Marshaler, array bool) *StreamEncoder {
+	return &StreamEncoder{
+		buf: bytes.NewBuffer(nil),
+		w: w,
+		first: true,
+		array: array,
+		quotedColumns: map[string][]byte{},
+		marshalFn: marshalFn,
+	}
+}
+
+var commaNl = []byte(",\n")
+
+func (sw *StreamEncoder) EncodeRow(row interface{}) error {
+	if !sw.first {
+		_, err := sw.buf.Write(commaNl)
+		if err != nil {
+			return fmt.Errorf("Failed to write comma: %s", err)
+		}
+	}
+
+	if sw.first && sw.array {
+		err := sw.buf.WriteByte('[')
+		if err != nil {
+			return err
+		}
+	}
+
+	sw.first = false
+
+	r, ok := row.(map[string]interface{})
+	if !ok {
+		// Short-circuit and fallback to marshalFn if this is not a map
+		bs, err := sw.marshalFn(row)
+		if err != nil {
+			return err
+		}
+
+		_, err = sw.buf.Write(bs)
+		return err
+	}
+
+	err := sw.buf.WriteByte('{')
+	if err != nil {
+		return err
+	}
+
+	j := -1
+	for col, val := range r {
+		j += 1
+
+		// Write a comma before the current key-value
+		if j > 0 {
+			err = sw.buf.WriteByte(',')
+			if err != nil {
+				return err
+			}
+		}
+
+		quoted := sw.quotedColumns[col]
+		if quoted == nil {
+			quoted = []byte(strconv.QuoteToASCII(col) + ":")
+			sw.quotedColumns[col] = quoted
+		}
+		_, err = sw.buf.Write(quoted)
+		if err != nil {
+			return err
+		}
+
+		bs, err := sw.marshalFn(val)
+		if err != nil {
+			return err
+		}
+
+		_, err = sw.buf.Write(bs)
+		if err != nil {
+			return err
+		}
+	}
+
+	return sw.buf.WriteByte('}')
+}
+
+func (sw *StreamEncoder) Close() error {
+	err := sw.buf.WriteByte(']')
+
+	for sw.buf.Len() > 0 {
+		_, err := sw.buf.WriteTo(sw.w)
+		if err != nil {
+			return err
+		}
+	}
+
+	return err
+}
+
+func EncodeGeneric(out io.Writer, obj interface{}, marshalFn Marshaler) error {
 	a, ok := obj.([]interface{})
 	// Fall back to normal encoder
 	if !ok {
@@ -29,91 +137,15 @@ func EncodeGeneric(out io.Writer, obj interface{}, marshalFn func(o interface{})
 		return nil
 	}
 
-	bo := bytes.NewBuffer(nil)
-	_, err := bo.Write([]byte("["))
-	if err != nil {
-		return err
-	}
-
-	quotedColumns := map[string][]byte{}
-
-	for i, row := range a {
-		// Write a comma before the current object
-		if i > 0 {
-			_, err = bo.Write([]byte(",\n"))
-			if err != nil {
-				return err
-			}
-		}
-
-		r, ok := row.(map[string]interface{})
-		if !ok {
-			bs, err := marshalFn(row)
-			if err != nil {
-				return err
-			}
-
-			_, err = bo.Write(bs)
-			if err != nil {
-				return err
-			}
-			continue
-		}
-
-		_, err := bo.Write([]byte("{"))
-		if err != nil {
-			return err
-		}
-
-		j := -1
-		for col, val := range r {
-			j += 1
-
-			// Write a comma before the current key-value
-			if j > 0 {
-				_, err = bo.Write([]byte(","))
-				if err != nil {
-					return err
-				}
-			}
-
-			quoted := quotedColumns[col]
-			if quoted == nil {
-				quoted = []byte(strconv.QuoteToASCII(col) + ":")
-				quotedColumns[col] = quoted
-			}
-			_, err = bo.Write(quoted)
-			if err != nil {
-				return err
-			}
-
-			bs, err := marshalFn(val)
-			if err != nil {
-				return err
-			}
-
-			_, err = bo.Write(bs)
-			if err != nil {
-				return err
-			}
-		}
-
-		_, err = bo.Write([]byte("}"))
+	encoder := NewStreamEncoder(out, marshalFn, true)	
+	for _, row := range a {
+		err := encoder.EncodeRow(row)
 		if err != nil {
 			return err
 		}
 	}
 
-	_, err = bo.Write([]byte("]"))
-
-	for bo.Len() > 0 {
-		_, err := bo.WriteTo(out)
-		if err != nil {
-			return err
-		}
-	}
-
-	return err
+	return encoder.Close()
 }
 
 func EncodeStdlib(out io.Writer, obj interface{}) error {
